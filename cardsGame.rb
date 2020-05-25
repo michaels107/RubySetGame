10c3921df6698da121f2b9ca5916da4fa012ea23
michaels107 <michaels.107@osu.edu>
2020-05-25 15:22:50 -0400

    Got it to prompt the user to play and then print out 12 cards starting at the top or i =0 in the deck into a grid.

--- cardsGame.rb
+++ cardsGame.rb
@@ -1,5 +1,31 @@
+# File created 5/25/2020 by Sean Michaels
 puts "Welcome to the Set Game!"
-score = 0 #keeps tracks of the amount of points you have scored in a game
+require_relative 'cards'
+name = Cards.new
+puts "Do you want to start playing[Y/N]:"
+ask = gets.chomp  #checks if the user wants to play the game, used later for replay.
+if ask.eql? "Y"
+  play = true
+else
+  play = false
+end
+while play
+  for i in 0..11 # prints the cards into 3 rows with 4 columns
+    card = name.play_deck[i]
+    if i%4 == 0 && i != 0
+      puts ""
+    end
+    print(card)
+    print(' ')
+  end
+  puts ""
 
-puts "Shuffling cards"
+  puts "Do you want to play again[Y/N]:" #for the user to replay
+  ask = gets.chomp
+  if ask.eql? "Y"
+    play = true
+  else
+    play = false
+  end
+end
 
