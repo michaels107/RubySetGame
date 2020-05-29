48b74d1615cf111e2f8cdae363e40b7a9fff7cc5
Duytan Tran <tran.732@osu.edu>
2020-05-28 23:06:35 -0400

    Subclass of Cards, Visualized, uses colorize gem and unicode to visualize set cards

--- cards.rb
+++ cards.rb
@@ -1,5 +1,7 @@
 # Created 5/22/2020 By Duytan Tran
 # Edited 5/26/2020 By Duytan Tran
+# Edited 5/28/2020 By Duytan Tran
+require 'colorize'
 =begin
 Class that generates two decks of 81 unique cards, one base-deck
 and one play-deck. The base deck contains an array of all 81 unique cards and
@@ -31,20 +33,103 @@ class Cards
 
   # Created 5/22/2020 By Duytan Tran
   # Edited 5/25/2020 By Duytan Tran: Reimplemented to be terse
+  # Edited 5/28/2020 By Duytan Tran: Reimplemented using reduce
   # Prints the cards in the base_deck
   def print_base_deck
     puts "Base deck: "
-    @base_deck.each_with_index { |card, i| puts if i != 0 && i % 10 == 0; print "#{card} " }
+    @base_deck.reduce 0 do |i, card|
+      puts if i != 0 && i % 10 == 0
+      print "#{card}\t"
+      i + 1
+      end
     puts
   end
 
   # Created 5/22/2020 By Duytan Tran
   # Edited 5/25/2020 By Duytan Tran: Reimplemented to be terse
+  # Edited 5/28/2020 By Duytan Tran: Reimplemented using reduce
   # Prints the cards in the play_deck
   def print_play_deck
     puts "Play deck: "
-    @play_deck.each_with_index { |card, i| puts if i != 0 && i % 10 == 0; print "#{card} " }
+    @play_deck.reduce 0 do |i, card|
+      puts if i != 0 && i % 10 == 0
+      print "#{card}\t"
+      i + 1 
+    end
     puts
+  end
+
+end
+
+# Created 5/28/2020 By Duytan Tran
+=begin
+Subclass of Cards class that includes visualizations of @base_deck and
+@play_deck. Featuring the use of the gem "colorize", the visualizations
+combine unicode characters with colors that form the shape, shading,
+color, and number properties of the Set card game. Also includes a method 
+that reshuffles @play_deck.
+=end
+class Visualized < Cards
+
+  # Created 5/28/2020 By Duytan Tran
+  def initialize
+
+    # Square shades
+    striped_square = "\u25A7".encode('utf-8')
+    open_square = "\u25A2".encode('utf-8')
+    solid_square = "\u25A0".encode('utf-8')
+
+    # Circle shades
+    striped_circle = "\u25CD".encode('utf-8')
+    open_circle = "\u25EF".encode('utf-8')
+    solid_circle = "\u2B24".encode('utf-8')
+
+    # Triangle shades
+    striped_triangle = "\u2A3A".encode('utf-8')
+    open_triangle = "\u25B3".encode('utf-8')
+    solid_triangle = "\u25B2".encode('utf-8')
+
+    # Building symbolic cards
+    super
+    @base_deck.map! do |card|
+
+      #Shape and shade cases
+      case card.slice 1, 2
+      when '11' then symbolized = striped_square
+      when '21' then symbolized = striped_circle
+      when '31' then symbolized = striped_triangle
+      when '12' then symbolized = open_square
+      when '22' then symbolized = open_circle
+      when '32' then symbolized = open_triangle
+      when '13' then symbolized = solid_square
+      when '23' then symbolized = solid_circle
+      else symbolized = solid_triangle
+      end
+
+      #Number of shapes
+      case card[0]
+      when '2' then symbolized = symbolized + " " + symbolized
+      when '3' then symbolized = symbolized + " " + symbolized + " " + symbolized
+      end
+
+      #Color cases
+      case card[3]
+      when '1' then symbolized = symbolized.colorize(:red)
+      when '2' then symbolized = symbolized.colorize(:green)
+      else symbolized = symbolized.colorize(:purple)
+      end
+      
+      #Concatenate symbolized version at the end of each card
+      card = symbolized
+
+    end
+    @play_deck = @base_deck.sample(81) #play_deck w/ symbols
+  end
+
+  # Created 5/28/2020 By Duytan Tran
+  # Shuffles a brand new @play_deck
+  def shuffle
+    @play_deck = @base_deck.sample(81)
   end
 
 end
