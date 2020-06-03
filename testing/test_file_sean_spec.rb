4675cf951defa3d05622e71cdc81807a22fc0677
michaels107 <michaels.107@osu.edu>
2020-06-02 21:39:22 -0400

    test file

---
+++ testing/test_file_sean_spec.rb
@@ -0,0 +1,23 @@
+# Created 6/2/2020 By Sean Michaels
+# Edited 6/2/2020 By Sean Michaels
+# Rspec test cases for the cards.rb source code.
+require_relative 'cards'
+require 'colorize'
+
+# Created 5/24/2020 By Duytan Tran
+# Test case 3: play_deck count
+describe Cards do
+  context "Testing that the tabled deck creating properly" do
+
+    it "should create a tabled deck that has the first 12 shuffled cards" do
+      set = Cards.new
+      first_twelve = []
+      (0..11).each {|i|
+        first_twelve.push set.play_deck[i]
+      }
+      t_cards = set.tabled_cards
+      expect(t_cards).to eq first_twelve
+    end
+
+  end
+end
